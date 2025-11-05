772a6af4c2e7d127092b15c1cfce87a3d19f2481
Gumercindo <gumercindojose@hotmail.com>
2025-11-05 17:07:25 -0300

    Inicial

---
+++ src/test/java/com/publico/htmltopdfapi/HtmlToPdfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publico.htmltopdfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlToPdfApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
