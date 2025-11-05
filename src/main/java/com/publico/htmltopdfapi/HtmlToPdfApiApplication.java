403adfdcae6c7ff4c819cdd9f513ca4147724ced
Gumercindo <gumercindojose@hotmail.com>
2025-11-05 17:08:39 -0300

    Inicial

--- src/main/java/com/publico/htmltopdfapi/HtmlToPdfApiApplication.java
+++ src/main/java/com/publico/htmltopdfapi/HtmlToPdfApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HtmlToPdfApiApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(HtmlToPdfApiApplication.class, args);
     }
 
